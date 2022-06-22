8dbc955672ce4905859569ae4b4e3af3d422002d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-06-22 06:18:18 +0200

    added abstract class

---
+++ src/main/java/abstract_class/A.java
@@ -0,0 +1,25 @@
+package abstract_class;
+
+public abstract class A {
+    abstract void m1();
+    abstract void m2();
+}
+class B extends A {
+    void m1(){
+        System.out.println("One");
+    }
+    void m2(){
+        System.out.println("Two");
+    }
+    void m3() {
+        System.out.println("Three");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        A a = new B();
+        a.m1();
+        a.m2();
+       // a.m3();//A do not have m2 method that is why we should create B
+    }
+}
