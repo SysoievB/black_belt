e5d5efec3687f7ab364b05d4ec46d7e36803c535
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-07-05 05:00:39 +0200

    added printf() & format()

--- src/main/java/abstract_class/A.java
+++ src/main/java/abstract_class/A.java
@@ -2,24 +2,29 @@ package abstract_class;
 
 public abstract class A {
     abstract void m1();
+
     abstract void m2();
 }
+
 class B extends A {
-    void m1(){
+    void m1() {
         System.out.println("One");
     }
-    void m2(){
+
+    void m2() {
         System.out.println("Two");
     }
+
     void m3() {
         System.out.println("Three");
     }
 }
+
 class Test {
     public static void main(String[] args) {
         A a = new B();
         a.m1();
         a.m2();
-       // a.m3();//A do not have m2 method that is why we should create B
+        // a.m3();//A do not have m2 method that is why we should create B
     }
 }
