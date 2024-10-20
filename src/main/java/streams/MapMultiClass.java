86c5388ed01be2783ce5649a0d81d4d6ba4bb246
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-20 15:24:10 +0200

    Added mapMulti operator

---
+++ src/main/java/streams/MapMultiClass.java
@@ -0,0 +1,22 @@
+package streams;
+
+import lombok.val;
+
+import java.util.List;
+
+public class MapMultiClass {
+    public static void main(String[] args) {
+        val list = List.of("hello", "hi", "ola", "привет");
+
+        val mapMultiList = list.stream()
+                .mapMulti((str, consumer) -> {
+                    if (str.length() <= 3) {
+                        consumer.accept(str + 100);
+                    } else {
+                        consumer.accept(str);
+                    }
+                })
+                .toList();
+        mapMultiList.forEach(System.out::println);
+    }
+}
