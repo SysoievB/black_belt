c37e7fbbd79a347cb15f96c25aa12cc63f9d9b11
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-29 18:39:03 +0100

    Added

--- src/main/java/concurrency_multithreading/multithreading/ThreadPoolExecutorServiceClass.java
+++ src/main/java/concurrency_multithreading/multithreading/ThreadPoolExecutorServiceClass.java
@@ -9,22 +9,17 @@ public class ThreadPoolExecutorServiceClass {
         System.out.println("main starts");
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 10; i++) {
-            executorService.execute(new RunnableImp());
+            executorService.execute(() -> {
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
         }
         executorService.shutdown();
         executorService.awaitTermination(5, TimeUnit.SECONDS);
         System.out.println("main ends");
-    }
-}
-
-class RunnableImp implements Runnable {
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName());
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
