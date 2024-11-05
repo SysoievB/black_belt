70a962dcc876c034fa00a54f0c353d6b9d32f76c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-05 06:38:54 +0100

    Added dining philosophers

--- src/main/java/concurrency_multithreading/course/tasks/DiningPhilosophersClass.java
+++ src/main/java/concurrency_multithreading/course/tasks/DiningPhilosophersClass.java
@@ -1,5 +1,11 @@
 package concurrency_multithreading.course.tasks;
 
+/**
+ * There are five silent philosophers (P1 – P5) sitting around a circular table, spending their lives eating and thinking.
+ * <p>
+ * There are five forks for them to share (1 – 5) and to be able to eat, a philosopher needs to have
+ * forks in both his hands. After eating, he puts both of them down and then they can be picked by another philosopher who repeats the same cycle.
+ */
 public class DiningPhilosophersClass {
     public static void main(String[] args) {
 
