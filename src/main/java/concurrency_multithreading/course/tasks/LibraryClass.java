d663aecc3a2a32459671680f3c4288a195c437a3
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-07 05:59:50 +0100

    Added library class

---
+++ src/main/java/concurrency_multithreading/course/tasks/LibraryClass.java
@@ -0,0 +1,15 @@
+package concurrency_multithreading.course.tasks;
+
+/**
+ * <h6>Task Description: </h6>
+ * There are 3 books and 5 students. Each student should read 1 book.
+ * Since there are fewer books than students, some books will need to be assigned to multiple
+ * students so that all books are actively being read by at least one student. The goal is to
+ * assign the books in such a way that each student has a book to read, and each book is read by
+ * at least one student.
+ * */
+public class LibraryClass {
+    public static void main(String[] args) {
+
+    }
+}
