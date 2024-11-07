363c090776f759ddd8486cc059d0994d0f7b401e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-07 19:03:46 +0100

    Added library class DONE

--- src/main/java/concurrency_multithreading/course/tasks/LibraryClass.java
+++ src/main/java/concurrency_multithreading/course/tasks/LibraryClass.java
@@ -1,8 +1,11 @@
 package concurrency_multithreading.course.tasks;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * <h6>Task Description: </h6>
@@ -11,22 +14,56 @@ import lombok.ToString;
  * students so that all books are actively being read by at least one student. The goal is to
  * assign the books in such a way that each student has a book to read, and each book is read by
  * at least one student.
- * */
+ */
 public class LibraryClass {
-    public static void main(String[] args) {
+    private static final BlockingQueue<Book> BOOKS = new LinkedBlockingQueue<>();
 
+    static {
+        BOOKS.add(new Book("First book"));
+        BOOKS.add(new Book("Second book"));
+        BOOKS.add(new Book("Third book"));
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new Student());
+        }
+
+        executorService.shutdown();
+    }
+
+    static Book getBook() throws InterruptedException {
+        return BOOKS.take();
+    }
+
+    static void returnBackBook(Book book) {
+        BOOKS.add(book);
     }
 }
+
 @Getter
 @ToString
 @AllArgsConstructor
 class Book {
     String title;
 }
+
 class Student implements Runnable {
 
     @Override
     public void run() {
+        try {
+            val book = LibraryClass.getBook();
+            System.out.println("Student with name: " + Thread.currentThread().getName() + " started reading book: " + book.title);
 
+            Thread.sleep(1000);
+
+            System.out.println("Student with name: " + Thread.currentThread().getName() + " finished reading book: " + book.title);
+            LibraryClass.returnBackBook(book);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
