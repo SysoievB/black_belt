49ba973120dcbc8d87159fc2d793d8d8f900a974
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-17 15:59:06 +0100

    Added  Hello World Multithreading

---
+++ src/main/java/concurrency_multithreading/course/tasks/BankAccountSimulatorClass.java
@@ -0,0 +1,72 @@
+package concurrency_multithreading.course.tasks;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Random;
+
+/**
+ * <h2>Task: Bank Account Simulator</h2>
+ * Description:
+ * Simulate a bank account with shared access by multiple threads. Create two threads,
+ * one for depositing and another for withdrawing money from the account. Ensure proper
+ * synchronization to avoid race conditions.
+ * <p>
+ * Specifications:
+ * The balance should not go negative.
+ * Use synchronized blocks or methods to manage the shared resource (the account).
+ * Goal: Understand shared resources and thread-safe operations.
+ */
+class BankAccountSimulatorClass {
+    public static void main(String[] args) throws InterruptedException {
+        val random = new Random();
+        val bank = new Bank(1000);
+
+        val deposit = new Thread(() -> {
+            for (int i = 0; i < random.nextInt(20); i++) {
+                val amount = random.nextInt(300);
+                System.out.println("Amount for depositing: " + amount);
+                bank.deposit(amount);
+                System.out.println(bank.getBalance());
+            }
+        });
+        deposit.start();
+
+        val withdraw = new Thread(() -> {
+            for (int i = 0; i < random.nextInt(20); i++) {
+                val amount = random.nextInt(500);
+                System.out.println("Amount for withdrawing: " + amount);
+                bank.withdraw(amount);
+                System.out.println(bank.getBalance());
+            }
+        });
+        withdraw.start();
+
+        // Ensure threads finish before program ends
+        deposit.join();
+        withdraw.join();
+
+        System.out.println("Final Balance: " + bank.getBalance());
+    }
+}
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+class Bank {
+    int balance;
+
+    synchronized int deposit(int amount) {
+        this.balance += amount;
+        return balance;
+    }
+
+    synchronized int withdraw(int amount) {
+        if (balance < amount) {
+            System.out.println("Insufficient funds for withdrawal of: " + amount);
+        } else {
+            this.balance -= amount;
+        }
+        return balance;
+    }
+}
