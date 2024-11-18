d1bb4547d69d2138f8c27af319ff5fb8c8027602
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-18 05:08:40 +0100

    Added  Producer-Consumer Problem task

---
+++ src/main/java/concurrency_multithreading/course/tasks/ProducerConsumerTaskClass.java
@@ -0,0 +1,16 @@
+package concurrency_multithreading.course.tasks;
+
+/**
+ * <h3>Task: Producer-Consumer Problem</h3>
+ * Description:
+ * Implement a producer-consumer problem using a shared buffer.
+ * <p>
+ * Specifications:
+ * Use a fixed-size buffer (e.g., an array of size 5).
+ * The producer thread adds items to the buffer.
+ * The consumer thread removes items from the buffer.
+ * Use wait() and notify() for inter-thread communication.
+ * Goal: Practice thread synchronization and learn to solve classic concurrency problems.
+ */
+class ProducerConsumerTaskClass {
+}
