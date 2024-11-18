5a9405686bea4c800e26427961ac15886c812918
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-18 20:21:39 +0100

    Added File search task

---
+++ src/main/java/concurrency_multithreading/course/tasks/FileSearchTask.java
@@ -0,0 +1,18 @@
+package concurrency_multithreading.course.tasks;
+
+/**
+ * <h3>Task: Multithreaded File Search</h3>
+ * Description:
+ * Write a program that searches for a specific word in a large directory containing multiple files.
+ * <p>
+ * Specifications:
+ * Use multiple threads to search through different files concurrently.
+ * Aggregate and display the results, including the file name and line number where the word is found.
+ * Use ExecutorService for thread management.
+ * Goal: Learn advanced thread management and how to combine results from concurrent threads.
+ */
+public class FileSearchTask {
+    public static void main(String[] args) {
+
+    }
+}
