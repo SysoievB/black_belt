4dc5c103ea6c90b7dfec2365b51b2988658814c4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-19 05:51:07 +0100

    Added Multithreaded Book Search task

--- src/main/java/concurrency_multithreading/course/tasks/FileSearchTask.java
+++ src/main/java/concurrency_multithreading/course/tasks/FileSearchTask.java
@@ -1,18 +1,48 @@
 package concurrency_multithreading.course.tasks;
 
+import lombok.val;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
 /**
- * <h3>Task: Multithreaded File Search</h3>
+ * <h3>Task: Multithreaded Book Search</h3>
  * Description:
- * Write a program that searches for a specific word in a large directory containing multiple files.
+ * Write a program that searches for a specific word in a large library containing multiple books.
  * <p>
  * Specifications:
- * Use multiple threads to search through different files concurrently.
- * Aggregate and display the results, including the file name and line number where the word is found.
+ * Use multiple threads to search through different books concurrently.
+ * Aggregate and display the results, including the book name where the word is found.
  * Use ExecutorService for thread management.
- * Goal: Learn advanced thread management and how to combine results from concurrent threads.
  */
 public class FileSearchTask {
-    public static void main(String[] args) {
+    private final static List<Book> LIBRARY = List.of(
+            new Book("To Kill a Mockingbird"),
+            new Book("1984"),
+            new Book("Pride and Prejudice"),
+            new Book("The Great Gatsby"),
+            new Book("Moby-Dick"),
+            new Book("War and Peace"),
+            new Book("The Catcher in the Rye"),
+            new Book("Crime and Punishment"),
+            new Book("The Lord of the Rings"),
+            new Book("The Chronicles of Narnia")
+    );
 
+    public static void main(String[] args) {
+        try (val service = Executors.newFixedThreadPool(2)){
+            service.submit(() -> search("an"))
+                    .get()
+                    .forEach(System.out::println);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Book> search(String bookName) {
+        return LIBRARY.stream()
+                .filter(book -> book.getTitle().toLowerCase().contains(bookName.toLowerCase()))
+                .toList();
     }
 }
