38ef1d6f6ba48c7214b358f3b3e91cff8d6d30c6
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-11-23 08:14:48 +0100

    Added CoffeeMachine task

--- src/main/java/concurrency_multithreading/course/tasks/CoffeeMachineTask.java
+++ src/main/java/concurrency_multithreading/course/tasks/CoffeeMachineTask.java
@@ -4,9 +4,22 @@ package concurrency_multithreading.course.tasks;
  * <h6>Coffee Machine Simulation</h6>
  * Description:
  * Simulate a simple coffee machine where one thread (the machine) prepares coffee and another thread
- * (the user) waits for the coffee to be ready. Use wait() and notify() for synchronization.*/
+ * (the user) waits for the coffee to be ready. Use wait() and notify() for synchronization.
+ */
 class CoffeeMachineTask {
     public static void main(String[] args) {
 
+    }
+}
+
+class CoffeeMachineThread extends Thread {
+    @Override
+    public void run() {
+    }
+}
+
+class UserThread extends Thread {
+    @Override
+    public void run() {
     }
 }
