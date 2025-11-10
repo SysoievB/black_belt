3590f44d142c3c0e6a07d45555d91731ad7ead87
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-11-10 08:15:07 +0100

    Added async

--- src/main/java/asynchronous/CallableAndFuture.java
+++ src/main/java/asynchronous/CallableAndFuture.java
@@ -1,15 +1,23 @@
 package asynchronous;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class CallableAndFuture {
     public static void main(String[] args) {
+        transformNumber(-5);
+        transformNumber(5);
+    }
+
+    private static void transformNumber(int number) {
         System.out.println("Main thread started...");
 
-        int number = 5;
+        Callable<Integer> callable = () -> {
+            if (number < 1) throw new Exception("Number must be greater than 0");
+            return number + 100;
+        };
 
-        Callable<Integer> callable = () -> number + 100;
         var futureTask = new FutureTask<>(callable);
         var thread = new Thread(futureTask);//futureTask - is a Runnable object
         thread.start();
@@ -19,8 +27,12 @@ public class CallableAndFuture {
             var getFuture = futureTask.get();
             System.out.printf("number of %d was transformed to %d\n", number, getFuture);
 
-        } catch (Exception e) {
-            System.out.printf("Main thread got an exception with message: %s%n", e.getMessage());
+        } catch (ExecutionException e) {//ExecutionException specific for FutureTask
+            var exception = futureTask.exceptionNow();
+            System.out.printf("Main thread got an exception: %s%n", e.getClass().getSimpleName());
+            System.out.printf("Exception: %s%n", exception.getMessage());
+        } catch (InterruptedException e) {
+            System.out.printf("Main thread got an interrupted exception with message: %s%n", e.getMessage());
         }
 
         System.out.println("Main thread finished...");
